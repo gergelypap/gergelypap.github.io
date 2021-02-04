15b6b94cca412bb8c69cc1f8ebcb836a7a52a175
Gergely Pap <pgege42@gmail.com>
2021-02-04 12:23:42 +0100

    Change some things, add tailwind, create basic scaffolding

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,26 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>@gpap</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias
+        aliquam, autem commodi cumque cupiditate deleniti dolor dolore doloribus
+        error est ex expedita hic incidunt inventore iure, laboriosam laborum
+        nam, non numquam officia quas quis recusandae rem repellendus sequi sit
+        tempora vel voluptates voluptatum. Aperiam deleniti dicta ducimus
+        eligendi enim esse excepturi, in itaque laudantium magnam nisi pariatur
+        porro temporibus totam, ut? Aliquid, cum cumque doloremque eaque
+        incidunt ipsum maxime nesciunt quo. A commodi, cupiditate distinctio
+        ducimus fugit ipsum, iure magni minima modi molestiae natus nisi nostrum
+        omnis perspiciatis provident quas quasi qui quos rem repellendus
+        temporibus veritatis vero voluptates?
+      </p>
+    </>
+  );
 }
