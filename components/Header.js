ba098ebab93f5e4311ece0fd8bb51fa1faf4712c
Gergely Pap <pgege42@gmail.com>
2021-02-04 19:04:32 +0100

    Testing workflow again

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,11 @@ import Nav from "components/Nav";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <picture className={styles.avatar}>
-        <source srcSet="/img/me.avif" type="image/avif" />
-        <img alt="@gpap" src="/img/me.webp" />
-      </picture>
+      <img
+        className={styles.avatar}
+        alt="@gpap"
+        src="https://avatars.githubusercontent.com/u/5883922?s=460&u=5d8abc013f98535fc97aea91f79e93dea71e7f54&v=4"
+      />
       <h1>@gpap</h1>
       <p>My developer blog. Because everybody has one.</p>
       <Nav />
--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 const path = require("path");
 
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
   poweredByHeader: false,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  exportPathMap: () => ({
+    "/": { page: "/" },
+  }),
+  assertPrefix: isProd
+    ? "https://cdn.statically.io/gh/gergelypap/gergelypap.github.io/gh-pages/"
+    : "",
 };
